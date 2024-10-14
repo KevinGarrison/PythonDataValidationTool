2220f7440c62ab13085da3020584ec11ee270602
Kevin Garrison <kevingarrison@outlook.de>
2024-10-14 21:53:19 +0200

    changed acceptance of file types

--- src/app.py
+++ src/app.py
@@ -5,7 +5,8 @@ st.title("Data Validation Tool")
 
 st.text("Upload your data below:")
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader(label="Choose a file", type=['csv', 'xls', 'xlsx'])
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.write(df)
+
